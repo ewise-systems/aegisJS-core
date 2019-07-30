c52268ee3e2fce07af71e818a1de29c52cdb253b
Arnel Hemady <ahemady@ewise.com>
2019-07-30 10:14:12 +0800

    support polling with delay

--- hos/pollingCore.js
+++ hos/pollingCore.js
@@ -1,6 +1,6 @@
 const { curry, equals, nth } = require("ramda");
 const { timer, throwError, interval , of} = require("rxjs");
-const { expand, bufferCount, take, takeWhile, retryWhen, mergeMap, filter, map, flatMap, catchError } = require("rxjs/operators");
+const { expand, bufferCount, take, takeWhile, retryWhen, mergeMap, filter, map, flatMap, catchError, delay } = require("rxjs/operators");
 
 // kickstart$ :: (a -> boolean) -> (_ -> Stream x) -> (_ -> Stream x) -> Stream x
 const kickstart$ = curry((retryLimit, retryDelay, pred, iStream, hStream) =>
@@ -25,16 +25,39 @@ const kickstart$ = curry((retryLimit, retryDelay, pred, iStream, hStream) =>
     )
 );
 
+// kickstart$ :: (a -> boolean) -> (_ -> Stream x) -> (_ -> Stream x) -> Stream x
+const kickstartwithdelay$ = curry((retryLimit, retryDelay, pred, iStream, hStream, pollingInterval) =>
+    iStream()
+    .pipe(
+        take(1),
+        expand(({processId: pid}) => hStream(pid).pipe(delay(pollingInterval))),
+        bufferCount(2, 1),
+        filter(([a, b]) => !equals(a, b)),
+        map(nth(1)),
+        retryWhen(err$ =>
+            err$.pipe(
+                mergeMap((err, i) =>
+                    i > retryLimit ||
+                    err === 500 ?
+                    throwError(err) :
+                    timer(retryDelay)
+                )
+            )
+        ),
+        takeWhile(pred, true),
+    )
+);
+
 //retryLimit -1 to always retry
 const kickstartpoll$ = curry((pollingInterval, retryLimit, retryDelay, pred, iStream) =>
-    interval(pollingInterval)
+    timer(0, pollingInterval)
     .pipe(
         flatMap(() => iStream()),
-        takeWhile(pred, true),
+        takeWhile(pred),
         retryWhen(err$ => 
             err$.pipe(
                 mergeMap((err, i) =>
-                    retryLimit != -1 && i > retryLimit ? throwError() : timer(retryDelay)
+                    retryLimit != -1 && i > retryLimit ? throwError(err) : timer(retryDelay)
                 )
             )
         ),
@@ -42,7 +65,7 @@ const kickstartpoll$ = curry((pollingInterval, retryLimit, retryDelay, pred, iSt
 );
 
 const kickstartpollstoponerror$ = curry((pollingInterval, pred, iStream) =>
-    interval(pollingInterval)
+    timer(0, pollingInterval)
     .pipe(
         flatMap(() => iStream()),
         takeWhile(pred, true),
@@ -52,6 +75,7 @@ const kickstartpollstoponerror$ = curry((pollingInterval, pred, iStream) =>
 
 module.exports = {
     kickstart$,
+    kickstartwithdelay$,
     kickstartpoll$,
     kickstartpollstoponerror$
 };
